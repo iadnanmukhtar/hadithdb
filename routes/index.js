91c04000cbefa77b567c4df8f41ee59ef8ddb84d
jarfile <adnanmukhtar@gmail.com>
2022-09-18 17:23:24 -0400

    show previous and next, fixed hilites

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const express = require('express');
 const LineReaderSync = require('line-reader-sync');
 const Hadith = require('../lib/Hadith');
 
-var books = JSON.parse(fs.readFileSync(__dirname + '/../data/books.json'));
-var db = [];
+global.books = JSON.parse(fs.readFileSync(__dirname + '/../data/books.json'));
+global.db = [];
+
 var dbsearchable = [];
 var recordReader = new LineReaderSync(__dirname + '/../data/hadiths-final.csv');
 for (var record = recordReader.readline(); record != null; record = recordReader.readline()) {
@@ -36,13 +37,13 @@ router.get('/', function (req, res, next) {
 });
 
 function searchWrapper(qs) {
-  if (qs.match(/^([^\s]+:\d+|\d+)/))
+  if (qs.match(/^([a-z]+:\d+|\d+)/))
     return searchNumber(qs);
   return search(toQueryPattern(qs));
 }
 
 function toQueryPattern(s) {
-  s = s.replace(/[\u064B\u064C\u064D\u064E\u064F\u0650\u0651\u0652\u0670]+/g, '');
+  s = removeDiacritics(s);
   s = s.replace(/[اءأآإ]/g, '[اءأآإ]');
   s = s.replace(/[يىئ]/g, '[يىئ]');
   s = s.replace(/[ؤو]/g, '[ؤو]');
@@ -72,7 +73,7 @@ function search(qs) {
     results = results.concat(searchQ(q).filter(function (value) {
       var add = true;
       for (var i = 0; i < results.length; i++) {
-        if (results[i].hadithNum == value.hadithNum) {
+        if (results[i].bookId == value.bookId && results[i].hadithNum == value.hadithNum) {
           add = false;
           break;
         }
@@ -94,7 +95,7 @@ function search(qs) {
     results = results.concat(searchQ(q).filter(function (value) {
       var add = true;
       for (var i = 0; i < results.length; i++) {
-        if (results[i].hadithNum == value.hadithNum) {
+        if (results[i].bookId == value.bookId && results[i].hadithNum == value.hadithNum) {
           add = false;
           break;
         }
@@ -113,9 +114,12 @@ function searchQ(q) {
     var recordPlain = dbsearchable[i];
     if (results.length > 1000)
       break;
+    var recordHilited = '';
     if (recordPlain.match(q)) {
       var re = new RegExp(q, 'gi');
       var m = null;
+      var hilites0 = [];
+      var hilites1 = [];
       while ((m = re.exec(recordPlain)) !== null) {
         var start = m.index;
         var end = m.index + m[0].length - 1;
@@ -124,25 +128,24 @@ function searchQ(q) {
           if (recordPlain[start - j].match(/[\s]/))
             break;
         if (j < 0) j = 0;
-        var startToks = recordPlain.substring(0, start - j).split(/ /).length;
+        hilites0.push(recordPlain.substring(0, start - j).split(/ /).length);
         // find ending token
         for (var j = 1; (end + j) < (recordPlain.length - 1); j++)
           if (recordPlain[end + j].match(/\s/))
             break;
         if (j >= recordPlain.length) j = recordPlain.length;
-        // hilite token
-        var endToks = recordPlain.substring(0, end + j).split(/ /).length - 1;
-        var recordHilited = '';
-        var toks = record.split(/ /);
-        for (var j = 0; j < toks.length; j++) {
-          if (j == startToks)
-            recordHilited += '<i>';
-          recordHilited += toks[j] + ' ';
-          if (j == endToks)
-            recordHilited += '</i>';
-        }
+        hilites1.push(recordPlain.substring(0, end + j).split(/ /).length - 1);
       }
-      results.push(new Hadith(recordHilited));
+      // hilite tokens
+      var toks = record.split(/ /);
+      for (var j = 0; j < toks.length; j++) {
+        if (hilites0.indexOf(j) >= 0)
+          recordHilited += '<i>';
+        recordHilited += toks[j] + ' ';
+        if (hilites1.indexOf(j) >= 0)
+          recordHilited += '</i>';
+      }
+      results.push(new Hadith(i, recordHilited));
     }
   }
   return results;
@@ -163,14 +166,14 @@ function searchNumber(q) {
     var re = new RegExp('^' + book[0].id + '\\t' + hadithNum + '([a-z]?|/[^\\t]*)\\t', 'i');
     for (var i = 0; i < db.length; i++) {
       if (db[i].match(re))
-        results.push(new Hadith(db[i]));
+        results.push(new Hadith(i, db[i]));
     }
   } else {
     hadithNum = toks[0];
     var re = new RegExp('^\\d+\\t' + hadithNum + '([a-z]?|/[^\\t]*)\\t', 'i');
     for (var i = 0; i < db.length; i++) {
       if (db[i].match(re))
-        results.push(new Hadith(db[i]));
+        results.push(new Hadith(i, db[i]));
     }
   }
   return results;
