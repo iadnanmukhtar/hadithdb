05a8978aa789319bd7f5cef1e5399a3af0ff5eee
jarfile <adnanmukhtar@gmail.com>
2022-09-21 01:50:36 -0400

    added grades for Ahmad

--- bin/utils/25794-grades.js
+++ bin/utils/25794-grades.js
@@ -12,7 +12,8 @@ const RE_REF = /\((\d)\) ?\.?$/;
 const RE_FOOTER_REF = /^\((\d)\) (.+?)[:،,\-\.]/;
 
 var lastHadithNum = 0;
-var hadiths = []
+var hadithTexts = [];
+var hadiths = [];
 var refs = [];
 var partialHadith = false;
 var withinFooter = false;
@@ -39,9 +40,6 @@ for (var pg = 152; pg < table.rows.length; pg++) {
 		var moreBody = withinPage && withinBody && !lines[ln + 1].startsWith('____');
 		var moreFooter = withinPage && withinFooter;
 
-		if (hadiths.indexOf('8') >= 0)
-			var x = 0;
-
 		if (withinFooter) {
 			// find hadith grade in footer
 			m = RE_FOOTER_REF.exec(line);
@@ -49,9 +47,10 @@ for (var pg = 152; pg < table.rows.length; pg++) {
 				if (refs.length > 0) {
 					var i = refs.indexOf(m[1]);
 					if (i >= 0) {
-						console.log(hadiths[i] + '\t' + m[2]);
+						console.log('8\t' + hadiths[i] + '\t' + m[2] + '\t' + hadithTexts[i]);
 						refs.splice(i, 1);
 						hadiths.splice(i, 1);
+						hadithTexts.splice(i, 1);
 
 						// var hadith = hadiths[i];
 						// var grade = m[2];
@@ -78,10 +77,13 @@ for (var pg = 152; pg < table.rows.length; pg++) {
 					refs.push(0);
 				partialHadith = false;
 				hadiths.push(m[1]);
+				hadithTexts.push(line.replace(RE_HADITH_NO_REF, ''));
 				refs.push(m[2]);
 				var hn = parseInt(m[1]);
+				if (hn == 2287)
+					hn = hn;
 				if ((hn - lastHadithNum) != 1)
-					console.log((lastHadithNum + 1) + '\tMissing');
+					console.log('8\t' + (lastHadithNum + 1) + '\tMissing');
 				lastHadithNum = hn;
 			} else {
 				m = RE_HADITH_NO_REF.exec(line);
@@ -90,12 +92,15 @@ for (var pg = 152; pg < table.rows.length; pg++) {
 						refs.push(0);
 					partialHadith = true;
 					hadiths.push(m[1]);
+					hadithTexts.push(line.replace(RE_HADITH_NO_REF, ''));
 					var hn = parseInt(m[1]);
 					if ((hn - lastHadithNum) != 1)
-						console.log((lastHadithNum + 1) + '\tMissing');
+						console.log('8\t' + (lastHadithNum + 1) + '\tMissing');
 					lastHadithNum = parseInt(m[1]);
 				} else if (partialHadith) {
 					m = RE_REF.exec(line);
+					if (!line.match(/^(مِن|ومِن|آخِرُ)?\s+(حَدِيث|مُسْنَد)/))
+						hadithTexts[hadithTexts.length - 1] += line;
 					if (m && m.length >= 2) {
 						if ((moreBody && lines[ln + 1].match(RE_HADITH_NO_REF)) || !moreBody) {
 							if (refs.indexOf(m[1]) < 0) {
@@ -111,8 +116,9 @@ for (var pg = 152; pg < table.rows.length; pg++) {
 	}
 	if (hadiths.length > 1) {
 		// unable to find grade for hadith
-		console.log(hadiths[0] + '\t' + 'Grade not found');
+		console.log('8\t' + hadiths[0] + '\t' + 'Grade not found\t' + hadithTexts[0]);
 		hadiths.splice(0, 1);
+		hadithTexts.splice(0, 1);
 	}
 
 }
--- bin/utils/hadiths-narrators.js
+++ bin/utils/hadiths-narrators.js
@@ -7,85 +7,89 @@ const { format } = require('@fast-csv/format');
 
 var lineReader = new LineReaderSync('data/archive/hadiths-final.csv');
 for (var line = lineReader.readline(); line != null; line = lineReader.readline()) {
+    try {
+        var toks = line.split(/\t/);
+        if (!toks) return;
+        var cid = toks[0];
+        var n = toks[1];
+        var g = toks[2];
+        var h_en = toks[3];
+        var h_ar = toks[4];
+        var ch = '';
+        var m = '';
+        if (h_en.startsWith("\""))
+            h_en = h_en.replace(/^"/, "").replace(/"$/, "");
+        h_en = h_en.replace(/\"{2,}/g, "\"");
 
-    var toks = line.split(/\t/);
-    if (!toks) return;
-    var cid = toks[0];
-    var n = toks[1];
-    var g = toks[2];
-    var h_en = toks[3];
-    var h_ar = toks[4];
-    var ch = '';
-    var m = '';
-    if (h_en.startsWith("\""))
-        h_en = h_en.replace(/^"/, "").replace(/"$/, "");
-    h_en = h_en.replace(/\"{2,}/g, "\"");
-
-    // normalize
-    h_ar = h_ar.replace(/[\:\"\'،۔ـ\-\.\,]/g, '');
-    h_ar = h_ar.replaceAll('رضى الله عنها', '');
-    h_ar = h_ar.replaceAll('رضى الله عنهما', '');
-    h_ar = h_ar.replaceAll('رضى الله عنهم', '');
-    h_ar = h_ar.replaceAll('رضى الله عنه', '');
-    h_ar = h_ar.replaceAll('صلى الله عليه وسلم', 'ﷺ');
-    h_ar = h_ar.replace(/\s+/g, ' ').trim();
-    var h2 = h_ar + '';
-    var m2 = '';
-    h2 = h2.replace(/[ؐ-ًؕ-ٖٓ-ٟۖ-ٰٰۭ]/g, '');
-    h2 = h2.replace(/و?(حدثنا|حدثني|حدثناه|حدثه) /g, '~ ');
-    h2 = h2.replace(/و?(أخبرنا|أخبرناه|أخبرني|أخبره) /g, '~ ');
-    h2 = h2.replace(/و?(سمعت|سمعنا|سمعناه|سمع) /g, '~ ');
-    h2 = h2.replace(/(عن|عنه|عنها) /g, '~ ');
-    h2 = h2.replace(/(يبلغ به) /g, '~~ ');
-    h2 = h2.replace(/(أنه|أن|أنها) /g, '~ ');
-    h2 = h2.replace(/(قال|قالت) /g, '~ ');
-    h2 = h2.replace(/\s+/g, ' ').trim();
-    // extract body
-    var narr_toks = h2.split(/~/);
-    if (narr_toks) {
-        var h2_tokslen = [];
-        narr_toks.forEach(tok => {
-            h2_tokslen.push(wordCount(tok));
-        });
-        for (var i = 0; i < narr_toks.length; i++) {
-            if (narr_toks[i].match(/(نبي|رسول)/)) {
-                m2 = narr_toks.slice(i).join('~ ');
-                break;
-            } else if (h2_tokslen[i] > 7 && !narr_toks[i].match(/ (بن|ابن) /)) {
-                m2 = narr_toks.slice(i).join('~ ');
-                break;
+        // normalize
+        h_ar = h_ar.replace(/[\:\"\'،۔ـ\-\.\,]/g, '');
+        h_ar = h_ar.replaceAll('رضى الله عنها', '');
+        h_ar = h_ar.replaceAll('رضى الله عنهما', '');
+        h_ar = h_ar.replaceAll('رضى الله عنهم', '');
+        h_ar = h_ar.replaceAll('رضى الله عنه', '');
+        h_ar = h_ar.replaceAll('صلى الله عليه وسلم', 'ﷺ');
+        h_ar = h_ar.replace(/\s+/g, ' ').trim();
+        var h2 = h_ar + '';
+        var m2 = '';
+        h2 = h2.replace(/[ؐ-ًؕ-ٖٓ-ٟۖ-ٰٰۭ]/g, '');
+        h2 = h2.replace(/و?(حدثنا|حدثني|حدثناه|حدثه) /g, '~ ');
+        h2 = h2.replace(/و?(أخبرنا|أخبرناه|أخبرني|أخبره) /g, '~ ');
+        h2 = h2.replace(/و?(سمعت|سمعنا|سمعناه|سمع) /g, '~ ');
+        h2 = h2.replace(/(عن|عنه|عنها) /g, '~ ');
+        h2 = h2.replace(/(يبلغ به) /g, '~~ ');
+        h2 = h2.replace(/(أنه|أن|أنها) /g, '~ ');
+        h2 = h2.replace(/(قال|قالت) /g, '~ ');
+        h2 = h2.replace(/\s+/g, ' ').trim();
+        // extract body
+        var narr_toks = h2.split(/~/);
+        if (narr_toks) {
+            var h2_tokslen = [];
+            narr_toks.forEach(tok => {
+                h2_tokslen.push(wordCount(tok));
+            });
+            for (var i = 0; i < narr_toks.length; i++) {
+                if (narr_toks[i].match(/(نبي|رسول)/)) {
+                    m2 = narr_toks.slice(i).join('~ ');
+                    break;
+                } else if (h2_tokslen[i] > 7 && !narr_toks[i].match(/ (بن|ابن) /)) {
+                    m2 = narr_toks.slice(i).join('~ ');
+                    break;
+                }
             }
+            if (m2 == '')
+                m2 = narr_toks[narr_toks.length - 1];
         }
-        if (m2 == '')
-            m2 = narr_toks[narr_toks.length - 1];
-    }
-    if (m2 == null) {
-        process.stdout.write('ERROR on: ' + cid + ' ' + n + '\n');
-        return;
-    }
-    m2 = m2.replace(/\s+/g, ' ').trim();
-
-    // extract chain and body
-    var h_toks = h_ar.split(/ /);
-    var h2_toks = h2.split(/ /);
-    var m2_toks = m2.split(/ /);
-    var m = '';
-    if (!h_toks || !m2_toks || h_toks.length == m2_toks.length)
-        m = h_ar;
-    else {
-        var diff = h_toks.length - m2_toks.length;
-        for (i = (diff - 1); i >= 0; i--) {
-            if (h2_toks[i].endsWith('~'))
-                diff--;
-            else
-                break;
+        if (m2 == null) {
+            process.stdout.write('ERROR on: ' + cid + ' ' + n + '\n');
+            return;
         }
-        ch = h_toks.slice(0, diff).join(' ').trim();
-        m = h_toks.slice(diff).join(' ').trim();
-    }
+        m2 = m2.replace(/\s+/g, ' ').trim();
 
-    // write
-    process.stdout.write(cid + '\t' + n + '\t' + g + '\t' + h_en + '\t' + h_ar + '\t' + ch + '\t' + m + '\n');
+        // extract chain and body
+        var h_toks = h_ar.split(/ /);
+        var h2_toks = h2.split(/ /);
+        var m2_toks = m2.split(/ /);
+        var m = '';
+        if (!h_toks || !m2_toks || h_toks.length == m2_toks.length)
+            m = h_ar;
+        else {
+            var diff = h_toks.length - m2_toks.length;
+            for (i = (diff - 1); i >= 0; i--) {
+                if (h2_toks[i].endsWith('~'))
+                    diff--;
+                else
+                    break;
+            }
+            ch = h_toks.slice(0, diff).join(' ').trim();
+            m = h_toks.slice(diff).join(' ').trim();
+        }
+
+        // write
+        process.stdout.write(cid + '\t' + n + '\t' + g + '\t' + h_en + '\t' + h_ar + '\t' + ch + '\t' + m + '\n');
+    } catch (err) {
+        console.log(err);
+        break;
+    }
 };
 
 function wordCount(s) {
