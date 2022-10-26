bf73c68f3a65adf65a07f0800b3d0ece521234f3
jarfile <adnanmukhtar@gmail.com>
2022-10-26 08:27:47 -0400

    general

--- README.md
+++ README.md
@@ -11,16 +11,18 @@
 The following nine plus source books of ḥadīth are currently availble:
 |#| Name | Graded | Translation |
 |-|------|--------|-------------|
+|0|The Holy Qurʾān|N/A|en|
 |1|Ṣaḥīḥ al-Bukhārī|Ṣaḥīḥ(en)|en|
 |2|Ṣaḥīḥ Muslim|Ṣaḥīḥ(en)|en|
 |3|Sunan al-Nasāʾī|Albānī(en)|en|
 |4|Sunan Abū Dawūd|Albānī(en)|en|
 |5|Jamiʿ al-Tirmidhī|Tirmidhī(ar), Albānī(en)|en|
 |6|Sunan Ibn Mājah|Albānī(en)|en|
-|7|Muwaṭṭaʾ Mālik|Ṣaḥīḥ(en)|en|
 |8|Musnad Aḥmad|Arnaʾūt(en)|N/A|
 |9|Sunan al-Dārimī|Dārānī(en)|N/A|
 |10|Mustadrak al-Ḥākim|Dhahabī(ar)|N/A|
+|11|Ibn Ḥibbān|N/A|N/A|
+|12|al-Muʿjam al-Kabīr of Ṭabarānī|N/A|N/A|
 
 ## TODO Enhancements
 - [ ]  Crowsdource translation editor
--- app.js
+++ app.js
@@ -88,8 +88,8 @@ global.graders = [{
 }];
 global.tags = [];
 
-global.connection = MySQL.createConnection(global.MySQLConfig.connection);
-global.query = Util.promisify(global.connection.query).bind(global.connection);
+global.dbPool = MySQL.createPool(global.MySQLConfig.connection);
+global.query = Util.promisify(global.dbPool.query).bind(global.dbPool);
 async function a_dbInitApp() {
   console.error('loading books...');
   global.books = await global.query('SELECT * FROM books ORDER BY id ASC');
@@ -100,6 +100,8 @@ async function a_dbInitApp() {
   console.error('loading graders...');
   global.graders = await global.query('SELECT * FROM graders');
   console.error('done loading hadith data');
+
+  var bookId = 8;
 
   // console.log('fix hadith decimal numbers');
   // var rows = await global.query(`SELECT * FROM hadiths WHERE num REGEXP "[^0-9]" ORDER BY bookId`);
@@ -113,36 +115,34 @@ async function a_dbInitApp() {
   //   `);
   // }
 
-  var bookId = 8;
-
-  // update hadiths heading numbers based on toc
-  var toc = await global.query(`SELECT * FROM toc WHERE bookId=${bookId} ORDER BY h1,h2,h3`);
-  var sql = '';
-  for (var i = 0; i < toc.length; i++) {
-    if (toc[i].h1 < 13)
-      continue;
-    if (i < (toc.length - 1)) {
-      sql = global.utils.sql(`
-        UPDATE hadiths
-        SET h1=${toc[i].h1},h2=${toc[i].h2},h3=${toc[i].h3}
-        WHERE bookId=${bookId} AND num0 BETWEEN ${toc[i].start0} AND ${toc[i + 1].start0}
-      `);
-      if (toc[i+1].start0 < toc[i].start0) {
-        console.error(sql);
-        console.error(toc[i+1]);
-        console.error(`next hadith range (${toc[i+1].start0}) is less than the previous (${toc[i].start0})`);
-        break;
-      }
-    } else {
-      sql = global.utils.sql(`
-        UPDATE hadiths
-        SET h1=${toc[i].h1},h2=${toc[i].h2},h3=${toc[i].h3}
-        WHERE bookId=${bookId} AND num0 >= ${toc[i].start0}
-      `);
-    }
-    console.log(sql);
-    await global.query(sql);
-  }
+  // // update hadiths heading numbers based on toc
+  // var toc = await global.query(`SELECT * FROM toc WHERE bookId=${bookId} ORDER BY h1,h2,h3`);
+  // var sql = '';
+  // for (var i = 0; i < toc.length; i++) {
+  //   if (toc[i].h1 < 13)
+  //     continue;
+  //   if (i < (toc.length - 1)) {
+  //     sql = global.utils.sql(`
+  //       UPDATE hadiths
+  //       SET h1=${toc[i].h1},h2=${toc[i].h2},h3=${toc[i].h3}
+  //       WHERE bookId=${bookId} AND num0 BETWEEN ${toc[i].start0} AND ${toc[i + 1].start0}
+  //     `);
+  //     if (toc[i+1].start0 < toc[i].start0) {
+  //       console.error(sql);
+  //       console.error(toc[i+1]);
+  //       console.error(`next hadith range (${toc[i+1].start0}) is less than the previous (${toc[i].start0})`);
+  //       break;
+  //     }
+  //   } else {
+  //     sql = global.utils.sql(`
+  //       UPDATE hadiths
+  //       SET h1=${toc[i].h1},h2=${toc[i].h2},h3=${toc[i].h3}
+  //       WHERE bookId=${bookId} AND num0 >= ${toc[i].start0}
+  //     `);
+  //   }
+  //   console.log(sql);
+  //   await global.query(sql);
+  // }
 
   // // update numInChapter
   // var prevH1 = 0;
