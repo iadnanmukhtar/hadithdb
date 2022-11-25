6a4ae7ac21dfb6b95b6620f8653de488dff16226
jarfile <adnanmukhtar@gmail.com>
2022-11-24 20:14:54 -0500

    fault tol

--- public/javascripts/express-admin-events.js
+++ public/javascripts/express-admin-events.js
@@ -6,22 +6,24 @@ const SearchIndex = require("../../lib/SearchIndex");
 exports.postSave = async function (req, res, args, next) {
 
 	var rows = await global.query(`SELECT bookId FROM hadiths WHERE id=${args.id[0]}`);
-	rows = await global.query(`
-		SELECT * FROM v_hadiths WHERE bookId=${rows[0].bookId} ORDER BY h1, numInChapter`);
+	var sql = `SELECT * FROM v_hadiths WHERE bookId=${rows[0].bookId} ORDER BY h1, numInChapter`;
+	rows = await global.query(sql);
+	console.log(sql); console.log(`results = ${rows.length}`);
 	var i = rows.findIndex(function (row) {
 		return (row.hId == parseInt(args.id[0]));
 	});
-	var data = {
-		_id: rows[i].hId
-	};
-	if (i > 0 && rows[i].bookId == rows[i - 1].bookId)
-		data.prevId = rows[i - 1].hId;
-	if (i < (rows.length - 1) && rows[i].bookId == rows[i + 1].bookId)
-		data.nextId = rows[i + 1].hId;
-	for (var k in rows[i])
-		data[k] = rows[i][k];
-	console.log(`PUT ${data.bookAlias}:${data.num}`);
-	await global.searchIdx.PUT([data], SearchIndex.TOKENIZER_OPTIONS);
-
+	if (i != undefined) {
+		var data = {
+			_id: rows[i].hId
+		};
+		if (i > 0 && rows[i].bookId == rows[i - 1].bookId)
+			data.prevId = rows[i - 1].hId;
+		if (i < (rows.length - 1) && rows[i].bookId == rows[i + 1].bookId)
+			data.nextId = rows[i + 1].hId;
+		for (var k in rows[i])
+			data[k] = rows[i][k];
+		console.log(`PUT ${data.bookAlias}:${data.num}`);
+		await global.searchIdx.PUT([data], SearchIndex.TOKENIZER_OPTIONS);
+	}
 	next();
 };
