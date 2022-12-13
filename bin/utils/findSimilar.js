863cb81098a606483cd69e21100a87c3a52b8d52
jarfile <adnanmukhtar@gmail.com>
2022-12-12 19:27:54 -0500

    find similar matches on hadith update

--- bin/utils/findSimilar.js
+++ bin/utils/findSimilar.js
@@ -4,87 +4,19 @@
 const HomeDir = require('os').homedir();
 const util = require('util');
 const MySQL = require('mysql');
-const Arabic = require('../../lib/Arabic');
-const stringSimilarity = require("string-similarity");
+const Hadith = require('../../lib/Hadith');
 
 const MySQLConfig = require(HomeDir + '/.hadithdb/store.json');
 var dbPool = MySQL.createPool(MySQLConfig.connection);
-var query = util.promisify(dbPool.query).bind(dbPool);
+global.query = util.promisify(dbPool.query).bind(dbPool);
 
 (async () => {
 	var go = false;
-	var rows = await getData();
+	var rows = await Hadith.a_dbGetDisemvoweledHadiths();
 	for (var i = 0; i < rows.length; i++) {
-		if (rows[i].bookId == 3 && rows[i].num0 == 760) go = true;
+		if (rows[i].bookId == 4 && rows[i].num0 == 2685) go = true;
 		if (!go) console.log(`skipping ${rows[i].bookId}:${rows[i].num0}`);
-		if (rows[i].body.length < 50 && (
-			rows[i].body.endsWith('مثله')
-		)) continue;
-		for (var j = 0; go && j < rows.length; j++) {
-			if (rows[i].id === rows[j].id) continue;
-			try {
-				var source = (rows[i].body.length >= rows[j].body.length) ? rows[j].body : rows[i].body;
-			var target = (rows[i].body.length >= rows[j].body.length) ? rows[i].body : rows[j].body;
-			var match = stringSimilarity.findBestMatch(source, [target]);
-				if (match.bestMatch.rating > 0.5) {
-					console.log(`${rows[i].bookId}:${rows[i].num0} ~ ${rows[j].bookId}:${rows[j].num0}\t${rows[i].body.substring(0, 100)}\t${rows[j].body.substring(0, 100)}`);
-					if (dbPool.state === 'disconnected') {
-						dbPool = MySQL.createPool(MySQLConfig.connection);
-						query = util.promisify(dbPool.query).bind(dbPool);
-					}
-					await query(`
-						INSERT INTO hadith_sim_candidates
-						VALUES (${rows[i].id}, ${rows[j].id}, ${match.bestMatch.rating})`);
-				}
-			} catch (err) {
-				console.error(`${rows[i].bookId}:${rows[i].num0} ~ ${rows[j].bookId}:${rows[j].num0}\t${err}`);
-			}
-		}
+		if (go) await Hadith.a_recordSimilarMatches(rows[i], rows);
 	}
 })();
 
-function getMatchRatio(findWords, withinWords) {
-	var stop = false;
-	var withinMatch = false;
-	var totalMatched = 0;
-	var totalMismatched = 0;
-	for (var i = 0; i < findWords.length && !stop; i++) {
-		for (var j = 0; j < withinWords.length && !stop; i++) {
-			var matched = matchWord(findWords[i], withinWords[i]);
-			if (matched) {
-				totalMatched++;
-				withinMatch = true;
-			} else
-				totalMismatched++;
-			if (withinMatch && totalMismatched > 2) stop = true;
-		}
-	}
-	
-}
-
-function matchWord(a, b) {
-	if (a === b)
-		return true;
-	else if (a.length >= 4 || b.length >= 4) {
-		a = a.substring(0, a.length - 1);
-		b = b.substring(0, b.length - 1);
-		if (a === b) return true;
-	}
-	return false;
-}
-
-async function getData() {
-	var rows = await query(`
-		SELECT id, bookId, num0, body FROM hadiths 
-		-- WHERE bookId=2 AND num0 >= 649.012
-		ORDER BY bookId, num0`);
-	rows = rows.map((row) => {
-		row.body = row.body.replace(/ًا/gu, '');
-		row.body = row.body.replace(/[^\p{L} ]/gu, '');
-		row.body = row.body.replace(/[ايوى]/g, '');
-		row.body = row.body.replace(/[ة]/g, 'ت');
-		row.body = row.body.replace(/[ؤئ]/g, 'ء');
-		return row;
-	});
-	return rows;
-}
--- public/javascripts/express-admin-events.js
+++ public/javascripts/express-admin-events.js
@@ -1,6 +1,7 @@
 /* jslint node:true, esversion:9 */
 'use strict';
 
+const Hadith = require("../../lib/Hadith");
 const SearchIndex = require("../../lib/SearchIndex");
 
 exports.postSave = async function (req, res, args, next) {
@@ -8,11 +9,12 @@ exports.postSave = async function (req, res, args, next) {
 	var rows = await global.query(`SELECT bookId FROM hadiths WHERE id=${args.id[0]}`);
 	var sql = `SELECT * FROM v_hadiths WHERE bookId=${rows[0].bookId} ORDER BY h1, numInChapter`;
 	rows = await global.query(sql);
-	console.log(sql); console.log(`results = ${rows.length}`);
 	var i = rows.findIndex(function (row) {
 		return (row.hId == parseInt(args.id[0]));
 	});
 	if (i != undefined) {
+
+		console.log(`post processing ${args.id[0]}`);
 		var data = {
 			_id: rows[i].hId
 		};
@@ -22,8 +24,17 @@ exports.postSave = async function (req, res, args, next) {
 			data.nextId = rows[i + 1].hId;
 		for (var k in rows[i])
 			data[k] = rows[i][k];
-		console.log(`PUT ${data.bookAlias}:${data.num}`);
+		console.log(`reindexed ${data.bookAlias}:${data.num}`);
 		await global.searchIdx.PUT([data], SearchIndex.TOKENIZER_OPTIONS);
+
+		console.log(`recording similar matches for ${data.bookAlias}:${data.num}`);
+		await global.query(`DELETE FROM hadith_sim_candidates WHERE hadithId1=${args.id[0]} OR hadithId2=${args.id[0]}`);
+		rows = await global.query(`SELECT * FROM hadiths WHERE id=${args.id[0]}`);
+		var deHadith = Hadith.disemvoweledHadith(rows[0]);
+		await Hadith.a_recordSimilarMatches(deHadith);
+	
 	}
+
+
 	next();
 };
