2d165386f84540128f589331213c867366e61692
jarfile <adnanmukhtar@gmail.com>
2022-12-30 18:56:28 -0500

    changed v_hadiths view

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ async function a_dbInitApp() {
   console.error('loading books...');
   global.books = await global.query('SELECT * FROM books ORDER BY id');
   console.error('loading quran...');
-  global.quran = await global.query('SELECT * FROM v_hadiths WHERE bookId=0 ORDER BY h1, numInChapter');
+  global.quran = await global.query('SELECT * FROM v_hadiths WHERE book_id=0 ORDER BY h1, numInChapter');
   for (var i = 0; i < global.quran.length; i++)
     global.quran[i].search_body = Arabic.normalize(Arabic.removeArabicDiacritics(global.quran[i].body));
   console.error('loading tags...');
