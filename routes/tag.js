f3861faba317a21fad42a31950465eac1f38f833
jarfile <adnanmukhtar@gmail.com>
2022-12-31 16:24:04 -0500

    paginate tag view

--- routes/tag.js
+++ routes/tag.js
@@ -6,7 +6,6 @@ const createError = require('http-errors');
 const asyncify = require('express-asyncify');
 const Hadith = require('../lib/Hadith');
 
-
 const router = asyncify(express.Router());
 
 router.get('/:tag', async function (req, res, next) {
@@ -17,11 +16,30 @@ router.get('/:tag', async function (req, res, next) {
   });
   if (!tag)
     throw createError(404, `Tag '${req.params.tag}' not found`);
-  var results = await Hadith.a_dbGetAllHadithsWithTag(tag.id);
+  
+  var offset = 0;
+  if (req.query.o)
+    offset = Math.floor(parseFloat(req.query.o) / global.MAX_PER_PAGE) * 100;
+  var results = await Hadith.a_dbGetAllHadithsWithTag(tag.id, offset);
+  if (req.query.o) {
+    results.pg = (Math.floor(parseFloat(req.query.o) / global.MAX_PER_PAGE)) + 1;
+    if (results.pg == 1)
+      delete results.pg;
+  }
+  if (offset > 0)
+    results.offset = offset;
+  results.hasNext = (results.length > global.MAX_PER_PAGE); results.pop();
+  results.prevOffset = ((offset - global.MAX_PER_PAGE) < global.MAX_PER_PAGE) ? 0 : offset - global.MAX_PER_PAGE;
+  results.nextOffset = offset + global.MAX_PER_PAGE;
+  results.hasPrev = ((offset - global.MAX_PER_PAGE) >= 0);
+  if (!results.hasNext)
+    delete results.nextOffset;
+  
   res.render('tag', {
     tag: tag,
     results: results
   });
+
 });
 
 module.exports = router;
