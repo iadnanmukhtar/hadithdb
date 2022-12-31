d4665e5f2947e9c7acd4472175bf08fc9ca7c9a7
jarfile <adnanmukhtar@gmail.com>
2022-12-31 11:40:03 -0500

    only reindex for hadith updates

--- public/javascripts/express-admin-events.js
+++ public/javascripts/express-admin-events.js
@@ -9,39 +9,42 @@ const SearchIndex = require("../../lib/SearchIndex");
 exports.postSave = async function (req, res, args, next) {
 	try {
 		console.log(`updated ${args.name}:${args.id[0]} by ${req.session.user.name}`);
-		
+
 		try {
 			await global.query(`UPDATE ${args.name} SET lastmod_user='${req.session.user.name}' WHERE id=${args.id[0]}`);
 		} catch (err) { }
 
-		var settings = JSON.parse(fs.readFileSync(HomeDir + '/.hadithdb/settings.json'));
-		if (settings.reindex || settings.findSimilar) {
+		if (args.name == 'hadiths') {
+			var settings = JSON.parse(fs.readFileSync(HomeDir + '/.hadithdb/settings.json'));
+			if (settings.reindex || settings.findSimilar) {
 
-			console.log(`post processing ${args.id[0]}`);
+				console.log(`post processing ${args.id[0]}`);
 
-			var rows = await global.query(`SELECT * FROM v_hadiths WHERE hId=${args.id[0]}`);
-			if (rows.length > 0) {
+				var rows = await global.query(`SELECT * FROM v_hadiths WHERE hId=${args.id[0]}`);
+				if (rows.length > 0) {
 
-				if (settings.reindex) {
-					var data = {
-						_id: rows[0].hId
-					};
-					delete rows[0].lastmod;
-					delete rows[0].highlight;
-					for (var k in rows[0])
-						data[k] = rows[0][k];
-					console.log(`reindexed ${data.book_alias}:${data.num}`);
-					await global.searchIdx.PUT([data], SearchIndex.TOKENIZER_OPTIONS);
+					if (settings.reindex) {
+						var data = {
+							_id: rows[0].hId
+						};
+						delete rows[0].lastmod;
+						delete rows[0].highlight;
+						for (var k in rows[0])
+							data[k] = rows[0][k];
+						console.log(`reindexed ${data.book_alias}:${data.num}`);
+						await global.searchIdx.PUT([data], SearchIndex.TOKENIZER_OPTIONS);
+					}
+					if (settings.findSimilar) {
+						rows[0].id = args.id[0];
+						console.log(`recording similar matches for ${rows[0].bookAlias}:${rows[0].num}`);
+						var deHadith = Hadith.disemvoweledHadith(rows[0]);
+						await Hadith.a_recordSimilarMatches(deHadith);
+					}
+
 				}
-				if (settings.findSimilar) {
-					rows[0].id = args.id[0];
-					console.log(`recording similar matches for ${rows[0].bookAlias}:${rows[0].num}`);
-					var deHadith = Hadith.disemvoweledHadith(rows[0]);
-					await Hadith.a_recordSimilarMatches(deHadith);
-				}
-	
 			}
 		}
+
 	} catch (err) {
 		console.error(err);
 	}
