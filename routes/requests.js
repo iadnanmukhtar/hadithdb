e4e430f69413d229e4b622304acfa8e9b05a869e
jarfile <adnanmukhtar@gmail.com>
2023-01-09 11:59:13 -0500

    add verified and translation requests

---
+++ routes/requests.js
@@ -0,0 +1,21 @@
+/* jslint node:true, esversion:9 */
+'use strict';
+
+const express = require('express');
+const asyncify = require('express-asyncify');
+const Hadith = require('../lib/Hadith');
+
+
+const router = asyncify(express.Router());
+
+router.get('/', async function (req, res, next) {
+  res.locals.req = req;
+  res.locals.res = res;
+  var results = [];
+  results = await Hadith.a_dbGetTranslationRequests();
+  res.render('requests', {
+    results: results
+  });
+});
+
+module.exports = router;
