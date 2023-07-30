ec33d59d8bd5f1a804097b609abe7fb5c18a16c1
jarfile <adnanmukhtar@gmail.com>
2023-07-30 14:29:29 -0400

    added an item reindex util

--- bin/buildSearchIndex.js
+++ bin/buildSearchIndex.js
@@ -1,38 +1,26 @@
 /* jslint node:true, esversion:9 */
 'use strict';
 
-const HomeDir = require('os').homedir();
+require('../lib/Globals');
 const fs = require('fs');
-const util = require('util');
-const MySQL = require('mysql');
 const Index = require('../lib/Index');
-
-const MySQLConfig = require(HomeDir + '/.hadithdb/store.json');
-var dbPool = MySQL.createPool(MySQLConfig.connection);
-var query = util.promisify(dbPool.query).bind(dbPool);
 
 (async () => {
 	try {
 		log(`retreiving data to index...`);
-		var books = await query(`SELECT * FROM books b ORDER BY id`);
+		var books = await global.query(`SELECT * FROM books b ORDER BY id`);
 		for (var i = 0; i < books.length; i++) {
 			// await indexDocs('hadiths', books[i]);
 			await indexDocs('toc', books[i]);
 		}
 	} finally {
-		dbPool.end();
+		global.dbPool.end();
 		log('indexing complete');
 	}
 })();
 
-async function indexDocs(indexName, book) {
-	log(`\n*****\ncreating ${indexName} index for ${book.shortName_en}...`);
-	var rows = await getData(indexName, book);
-	await Index.updateBulk(indexName, rows);
-}
-
 async function getData(indexName, book) {
-	var rows = await query(`
+	var rows = await global.query(`
 		SELECT * FROM v_${indexName}
 		WHERE
 			book_id = ${book.id}
@@ -40,6 +28,12 @@ async function getData(indexName, book) {
 			book_id, h1, numInChapter
 		-- LIMIT 10`);
 	return rows;
+}
+
+async function indexDocs(indexName, book) {
+	log(`\n*****\ncreating ${indexName} index for ${book.shortName_en}...`);
+	var rows = await getData(indexName, book);
+	await Index.updateBulk(indexName, rows);
 }
 
 function log(message) {
---
+++ bin/reindexItems.js
@@ -0,0 +1,49 @@
+/* jslint node:true, esversion:9 */
+'use strict';
+
+require('../lib/Globals');
+const fs = require('fs');
+const Index = require('../lib/Index');
+const { Item, Heading } = require('../lib/Model');
+
+const date = '2023-07-30';
+
+(async () => {
+	try {
+		await reindexItems();
+		await reindexTOC();
+	} finally {
+		global.dbPool.end();
+		logfile('indexing complete');
+	}
+})();
+
+async function reindexItems() {
+	var items = await global.query(`SELECT * FROM v_hadiths
+		WHERE lastmod >= '${date}'
+		ORDER BY ordinal`);
+	await indexDocs(Item.INDEX, items);
+}
+
+async function reindexTOC() {
+	var headings = await global.query(`SELECT * FROM v_toc
+		WHERE lastmod >= '${date}'
+		ORDER BY ordinal`);
+	await indexDocs(Heading.INDEX, headings);
+	var items = await global.query(`SELECT h.* FROM toc t, v_hadiths h
+		WHERE t.id = h.tId AND t.lastmod >= '${date}'
+		ORDER BY t.ordinal`);
+	await indexDocs(Item.INDEX, items);
+}
+
+
+
+async function indexDocs(indexName, recs) {
+	logfile(`\n*****\nreindexing ${recs.length} records in index ${indexName}...`);
+	await Index.updateBulk(indexName, recs);
+}
+
+function logfile(message) {
+	console.log(message);
+	fs.appendFileSync('reindexItems.log', message + '\n');
+}
