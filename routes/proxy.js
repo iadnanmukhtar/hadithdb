57354a472627afa3e5ab7bc41b14996e01d30e30
jarfile <adnanmukhtar@gmail.com>
2024-06-15 14:18:31 -0400

    .

--- routes/proxy.js
+++ routes/proxy.js
@@ -4,6 +4,7 @@ const debug = require('debug')('hadithdb:proxy');
 const express = require('express');
 const asyncify = require('express-asyncify').default;
 const axios = require('axios');
+const https = require('https')
 
 const router = asyncify(express.Router());
 
@@ -12,16 +13,19 @@ router.get('/:url', async function (req, res, next) {
   res.locals.res = res;
   debug(`Proxy: ${req.params.url}`);
   try {
-    fetch(req.params.url, { method: 'GET', headers: req.headers })
-      .then(resource => resource.json())
-      .then(resource => {
-        res.send(resource);
-        return;
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send(error);
-      });
+    const agent = new https.Agent({
+      rejectUnauthorized: false
+    });
+    fetch(req.params.url, { method: 'GET', headers: req.headers, agent: agent })
+       .then(resource => resource.json())
+       .then(resource => {
+         res.send(resource);
+         return;
+       })
+       .catch(error => {
+         console.log(error);
+         res.status(500).send(error);
+       });
   } catch (e) {
     console.log(`Proxy: ${req.params.url} ${e}`);
     throw new ReferenceError(`Proxy: ${req.params.url} ${e}`);
