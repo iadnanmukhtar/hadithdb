b50cac6ca425adf0533c9134e2129723e667b5aa
jarfile <adnanmukhtar@gmail.com>
2024-07-08 15:18:39 -0400

    wait for all images before images are displayed

--- routes/proxy.js
+++ routes/proxy.js
@@ -27,22 +27,9 @@ router.get('/:url', async function (req, res, next) {
     } catch (e) {
       console.log(e);
     }
-    console.log(text);
     res.send(text);
     res.end();
     return;
-    // fetch(req.params.url, { method: 'GET', headers: req.headers, agent: agent })
-    //    .then(resource => resource.json())
-    //    .then(resource => {
-    //      res.send(resource);
-    //      res.end();
-    //      return;
-    //    })
-    //    .catch(error => {
-    //      console.log(error);
-    //      res.status(500).send(error);
-    //      res.end();
-    //    });
   } catch (e) {
     console.log(`Proxy: ${req.params.url} ${e}`);
     throw new ReferenceError(`Proxy: ${req.params.url} ${e}`);
