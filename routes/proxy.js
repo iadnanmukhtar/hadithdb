ad9b5afd62cebc912e5f43a7cdb1c859ec372f38
jarfile <adnanmukhtar@gmail.com>
2024-07-08 13:56:36 -0400

    .

--- routes/proxy.js
+++ routes/proxy.js
@@ -16,18 +16,24 @@ router.get('/:url', async function (req, res, next) {
     const agent = new https.Agent({
       rejectUnauthorized: false
     });
-    fetch(req.params.url, { method: 'GET', headers: req.headers, agent: agent })
-       .then(resource => resource.json())
-       .then(resource => {
-         res.send(resource);
-         res.end();
-         return;
-       })
-       .catch(error => {
-         console.log(error);
-         res.status(500).send(error);
-         res.end();
-       });
+    const resource = await fetch(req.params.url, { method: 'GET', headers: req.headers, agent: agent });
+    const text = await resource.text();
+    console.log(text);
+    res.send(text);
+    res.end();
+    return;
+    // fetch(req.params.url, { method: 'GET', headers: req.headers, agent: agent })
+    //    .then(resource => resource.json())
+    //    .then(resource => {
+    //      res.send(resource);
+    //      res.end();
+    //      return;
+    //    })
+    //    .catch(error => {
+    //      console.log(error);
+    //      res.status(500).send(error);
+    //      res.end();
+    //    });
   } catch (e) {
     console.log(`Proxy: ${req.params.url} ${e}`);
     throw new ReferenceError(`Proxy: ${req.params.url} ${e}`);
